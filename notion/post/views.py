235bc5e32dd134a99e2bc28adf2bc4d6c6dffa69
awds1236 <awds1236@gmail.com>
2023-09-01 19:59:15 +0900

    ORM > Raw 쿼리로 변경

--- notion/post/views.py
+++ notion/post/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Page
+from django.db import connection
 from .forms import PostForm
 
 # 게시물 목록 조회
 def post_list(request):
-    posts = Page.objects.filter(parent_page=None)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM post_page WHERE parent_page_id IS NULL")
+        posts = cursor.fetchall()
+
     return render(request, 'post_list.html', {'posts': posts})
 
-# 브로드 크럼스 (model의 외래키를 사용하여 생성)
-def get_breadcrumbs(post):
+
+# 브로드 크럼스
+def get_breadcrumbs(post_id):
     crumbs = []
-    while post:
-        crumbs.append(post)
-        post = post.parent_page
+    with connection.cursor() as cursor:
+        while post_id:
+            cursor.execute("SELECT id, title, content, parent_page_id FROM post_page WHERE id = %s", [post_id])
+            post = cursor.fetchone()
+            if post:
+                crumbs.append(post)
+                post_id = post[3]  # parent_page_id의 index
+            else:
+                post_id = None
+
     return crumbs[::-1]
 
 # 부모 게시물 생성
@@ -20,30 +32,47 @@ def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO post_page (title, content) VALUES (%s, %s)", [title, content])
+            
             return redirect('post_list')
     else:
         form = PostForm()
+
     return render(request, 'create_post.html', {'form': form})
 
 # 게시물 상세 조회, 서브 페이지 조회
 def post_detail(request, post_id):
-    post = get_object_or_404(Page, id=post_id)
-    breadcrumbs = get_breadcrumbs(post)
-    sub_pages = Page.objects.filter(parent_page=post)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM post_page WHERE id = %s", [post_id])
+        post = cursor.fetchone()
+
+        cursor.execute("SELECT * FROM post_page WHERE parent_page_id = %s", [post_id])
+        sub_pages = cursor.fetchall()
+
+    breadcrumbs = get_breadcrumbs(post_id)
     return render(request, 'post_detail.html', {'page': post, 'breadcrumbs': breadcrumbs, 'sub_pages': sub_pages})
 
 # 서브 페이지 생성
 def create_sub_post(request, post_id):
-    parent_post = get_object_or_404(Page, id=post_id)
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            sub_post = form.save(commit=False)
-            sub_post.parent_page = parent_post
-            sub_post.save()
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO post_page (title, content, parent_page_id) VALUES (%s, %s, %s)", [title, content, post_id])
+            
             return redirect('post_detail', post_id=post_id)
     else:
         form = PostForm()
-    return render(request, 'create_sub_post.html', {'form': form, 'page': parent_post})
 
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM post_page WHERE id = %s", [post_id])
+        parent_post = cursor.fetchone()
+
+    return render(request, 'create_sub_post.html', {'form': form, 'page': parent_post})
